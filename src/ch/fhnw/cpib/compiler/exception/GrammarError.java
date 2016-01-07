e1766166e6fe6ece1fe0af06e2e3ff2e08079e23
Patrick Schmid <patrick.p.schmid@gmail.com>
2016-01-07 19:40:56 +0100

    corrected typo and formating for error messages

--- src/ch/fhnw/cpib/compiler/exception/GrammarError.java
+++ src/ch/fhnw/cpib/compiler/exception/GrammarError.java
@@ -3,7 +3,7 @@ package ch.fhnw.cpib.compiler.exception;
 public class GrammarError extends Exception{
     
     public GrammarError(String message, int lineNumber){
-    	System.out.println("["+ lineNumber + " - ContextError] " + message);
+    	System.out.println("["+ lineNumber + " - GrammarError] " + message);
     	System.exit(1);
     }
 }
--- src/ch/fhnw/cpib/compiler/exception/LexicalError.java
+++ src/ch/fhnw/cpib/compiler/exception/LexicalError.java
@@ -4,23 +4,8 @@ public class LexicalError extends Exception {
 
     private static final long serialVersionUID = -7199114117172422355L;
 
-    private final int lineNumber;
-    private final int charNumber;
-    
-    private final char c;
-    
-    private final String message;
-    
     public LexicalError(String message, char c, int lineNumber, int charNumber) {
-        super();
-        
-        this.lineNumber = lineNumber;
-        this.message = message;
-        this.charNumber = charNumber;
-        this.c = c;
-    }
-    
-    public String getMessage() {
-        return "["+ lineNumber + ":" + charNumber + " - LexicalError] " + message + " " + c;
+    	System.out.println("["+ lineNumber + ":" + charNumber + " - LexicalError] " + message + ": " + c);
+    	System.exit(1);
     }
 }
